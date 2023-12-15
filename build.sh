7db1351cb5a6885e28356e0de0656edac4987589
SunBK201 <sunbk201gm@gmail.com>
2023-12-16 01:22:41 +0800

    chore: build trimpath

--- build.sh
+++ build.sh
@@ -15,19 +15,19 @@ gofmt -w ./
 for goarch in "amd64" "arm" "arm64" "mipsle" "mips64" "riscv64" "386"; do
     obj_name=$project_name
 
-    GOOS=linux GOARCH=$goarch go build -ldflags="-s -w" $target
+    GOOS=linux GOARCH=$goarch go build -trimpath -ldflags="-s -w" $target
     tar -zcf $release_dir/$project_name-$release_version-$goarch.tar.gz $obj_name
     rm -f $obj_name
 done
 
-GOOS=linux GOARCH="mipsle" GOMIPS=softfloat go build -ldflags="-s -w" $target
+GOOS=linux GOARCH="mipsle" GOMIPS=softfloat go build -trimpath -ldflags="-s -w" $target
 tar -zcf $release_dir/$project_name-$release_version-mipsle-softfloat.tar.gz $obj_name
 rm -f $obj_name
-GOOS=linux GOARCH="mipsle" GOMIPS=hardfloat go build -ldflags="-s -w" $target
+GOOS=linux GOARCH="mipsle" GOMIPS=hardfloat go build -trimpath -ldflags="-s -w" $target
 tar -zcf $release_dir/$project_name-$release_version-mipsle-hardfloat.tar.gz $obj_name
 rm -f $obj_name
 
-GOOS=linux GOARCH="arm" GOARM=7 go build -ldflags="-s -w" $target
+GOOS=linux GOARCH="arm" GOARM=7 go build -trimpath -ldflags="-s -w" $target
 tar -zcf $release_dir/$project_name-$release_version-armv7.tar.gz $obj_name
 rm -f $obj_name
 
