65f0cbfefc70219b300b67de83577b42e2c151d4
SunBK201 <sunbk201gm@gmail.com>
2023-12-21 22:00:28 +0800

    feat: support luci

---
+++ luci/cbi.lua
@@ -0,0 +1,50 @@
+local uci = require("luci.model.uci").cursor()
+
+ua3f = Map("ua3f",
+    "UA3F",
+    [[
+        <a href="https://github.com/SunBK201/UA3F" target="_blank">Version: 0.1.3</a>
+        <br>
+        Across the Campus we can reach every corner in the world.
+    ]]
+)
+
+enable = ua3f:section(NamedSection, "enabled", "ua3f", "UA3F Status")
+main = ua3f:section(NamedSection, "main", "ua3f", "UA3F Settings")
+
+enable:option(Flag, "enabled", "Enabled")
+status = enable:option(DummyValue, "status", "Status")
+status.rawhtml = true
+status.cfgvalue = function(self, section)
+    local pid = luci.sys.exec("pidof ua3f")
+    if pid == "" then
+        return "<span style='color:red'>" .. translate("Disabled") .. "</span>"
+    else
+        return "<span style='color:green'>" .. translate("Enabled") .. "</span>"
+    end
+end
+
+main:tab("general", "General Settings")
+main:tab("log", "Log")
+
+port = main:taboption("general", Value, "port", "Port")
+port.placeholder = "1080"
+bind = main:taboption("general", Value, "bind", "Bind Address")
+bind:value("127.0.0.1")
+bind:value("0.0.0.0")
+ua = main:taboption("general", Value, "ua", "User-Agent")
+ua.placeholder = "FFF"
+log_level = main:taboption("general", ListValue, "log_level", "Log Level")
+log_level:value("debug")
+log_level:value("info")
+log_level:value("warn")
+log_level:value("error")
+
+log = main:taboption("log", TextValue, "")
+log.readonly = true
+log.cfgvalue = function(self, section)
+    return luci.sys.exec("cat /var/log/ua3f.log")
+end
+log.rows = 30
+
+return ua3f
