8b30efafd61f950beb46622ee72b52456ed1b5fb
SunBK201 <sunbk201gm@gmail.com>
2024-02-27 14:35:10 +0800

    fix: shellclash username not found

--- cmd/ua3f.go
+++ cmd/ua3f.go
@@ -17,7 +17,7 @@ import (
 	"github.com/sirupsen/logrus"
 )
 
-var version = "0.2.2"
+var version = "0.2.3"
 var payloadByte []byte
 var cache *expirable.LRU[string, string]
 var HTTP_METHOD = []string{"GET", "POST", "HEAD", "PUT", "DELETE", "OPTIONS", "TRACE", "CONNECT"}
