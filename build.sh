e34de96116825f14ccdea3fc69e8a65c57446c2a
SunBK201 <sunbk201gm@gmail.com>
2024-11-08 22:02:59 +0800

    chore: tidy build script

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 project_name="ua3f"
 release_version="0.5.1"
-target=src/main.go
+target=main.go
 dist=./dist
 release_dir=./bin
 
@@ -14,46 +14,46 @@ mkdir -p $dist/bin
 cd $(dirname $0)
 
 gofmt -w ./
-
+cd $(dirname $0)/src
 for goarch in "amd64" "arm" "arm64" "mipsle" "mips64" "riscv64" "386"; do
     obj_name=$project_name-$release_version-$goarch
 
     GOOS=linux GOARCH=$goarch go build -trimpath -ldflags="-s -w" -o $obj_name $target
-    cp $obj_name $dist/bin
+    cp $obj_name ../dist/bin/
     mv $obj_name $project_name
-    tar -zcf $release_dir/$project_name-$release_version-$goarch.tar.gz $project_name
+    tar -zcf ../bin/$project_name-$release_version-$goarch.tar.gz $project_name
     rm -f $project_name
 done
 
 # mipsle-softfloat
 obj_name=$project_name-$release_version-mipsle-softfloat
 GOOS=linux GOARCH="mipsle" GOMIPS=softfloat go build -trimpath -ldflags="-s -w" -o $obj_name $target
-cp $obj_name $dist/bin
+cp $obj_name ../dist/bin/
 mv $obj_name $project_name
-tar -zcf $release_dir/$project_name-$release_version-mipsle-softfloat.tar.gz $project_name
+tar -zcf ../bin/$project_name-$release_version-mipsle-softfloat.tar.gz $project_name
 rm -f $project_name
 
 # mipsle-hardfloat
 obj_name=$project_name-$release_version-mipsle-hardfloat
 GOOS=linux GOARCH="mipsle" GOMIPS=hardfloat go build -trimpath -ldflags="-s -w" -o $obj_name $target
-cp $obj_name $dist/bin
+cp $obj_name ../dist/bin/
 mv $obj_name $project_name
-tar -zcf $release_dir/$project_name-$release_version-mipsle-hardfloat.tar.gz $project_name
+tar -zcf ../bin/$project_name-$release_version-mipsle-hardfloat.tar.gz $project_name
 rm -f $project_name
 
 # armv7
 obj_name=$project_name-$release_version-armv7
 GOOS=linux GOARCH="arm" GOARM=7 go build -trimpath -ldflags="-s -w" -o $obj_name $target
-cp $obj_name $dist/bin
+cp $obj_name ../dist/bin/
 mv $obj_name $project_name
-tar -zcf $release_dir/$project_name-$release_version-armv7.tar.gz $project_name
+tar -zcf ../bin/$project_name-$release_version-armv7.tar.gz $project_name
 rm -f $project_name
 
 # armv8
-cp $release_dir/$project_name-$release_version-arm64.tar.gz $release_dir/$project_name-$release_version-armv8.tar.gz
-cp $dist/bin/$project_name-$release_version-arm64 $dist/bin/$project_name-$release_version-armv8
+cp ../bin/$project_name-$release_version-arm64.tar.gz ../bin/$project_name-$release_version-armv8.tar.gz
+cp ../dist/bin/$project_name-$release_version-arm64 ../dist/bin/$project_name-$release_version-armv8
 
-cd $release_dir
+cd ../bin
 for file in ./*; do
     md5 -r $file >>sha1sum.txt
 done
