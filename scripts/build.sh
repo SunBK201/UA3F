4598ac7fb749b7015fed697c04b6bcd0e1037ac3
SunBK201 <sunbk201gm@gmail.com>
2025-11-30 20:48:29 +0800

    chore: bump version to 1.9.1

--- scripts/build.sh
+++ scripts/build.sh
@@ -8,7 +8,7 @@
 set -e
 
 project_name="ua3f"
-release_version="1.9.0"
+release_version="1.9.1"
 target=main.go
 
 LINUX_ARCHS="amd64 arm arm64 mipsle mips64 riscv64 386 mipsle-softfloat mipsle-hardfloat armv7 armv8"
