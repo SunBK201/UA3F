fcc4fc6440c55ff67e30f940efa0b66b1571ea7b
SunBK201 <sunbk201gm@gmail.com>
2025-12-27 02:48:17 +0800

    refactor: remove redundant log message for Nfqueue worker start

--- src/internal/server/base/nfqueue.go
+++ src/internal/server/base/nfqueue.go
@@ -144,7 +144,6 @@ func (s *NfqueueServer) Close() {
 func (s *NfqueueServer) worker(workerID int, aChan <-chan *nfq.Attribute) {
 	defer s.wg.Done()
 	defer slog.Info("Nfqueue worker stopped", slog.Int("workerID", workerID))
-	slog.Info("Nfqueue worker started", slog.Int("workerID", workerID))
 
 	for a := range aChan {
 		if ok := attributeSanityCheck(a); !ok {
