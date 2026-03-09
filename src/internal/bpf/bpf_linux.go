2a8c5be66a6b45d131fb270b1ecaad409686517e
SunBK201 <sunbk201gm@gmail.com>
2026-03-10 02:14:07 +0800

    fix: replace custom logging with slog

--- src/internal/bpf/bpf_linux.go
+++ src/internal/bpf/bpf_linux.go
@@ -4,6 +4,7 @@ package bpf
 
 import (
 	"io"
+	"log/slog"
 
 	"github.com/sunbk201/ua3f/internal/common"
 	"github.com/sunbk201/ua3f/internal/config"
@@ -68,12 +69,12 @@ func (b *BPF) TryOffload(c *common.ConnLink, drainReader io.Reader) bool {
 			n, err := io.ReadFull(drainReader, buf)
 			if n > 0 {
 				if _, werr := c.RConn.Write(buf[:n]); werr != nil {
-					c.LogWarnf("BPF offload: drain write error: %v", werr)
+					slog.Warn("BPF offload: drain write error", "error", werr, "ConnLink", c)
 					return false
 				}
 			}
 			if err != nil && err != io.ErrUnexpectedEOF {
-				c.LogWarnf("BPF offload: drain read error: %v", err)
+				slog.Warn("BPF offload: drain read error", "error", err, "ConnLink", c)
 				return false
 			}
 		}
@@ -81,32 +82,32 @@ func (b *BPF) TryOffload(c *common.ConnLink, drainReader io.Reader) bool {
 
 	lfd, err := c.LFD()
 	if err != nil {
-		c.LogWarnf("BPF offload: LFD: %v", err)
+		slog.Warn("BPF offload: LFD error", "error", err, "ConnLink", c)
 		return false
 	}
 	rfd, err := c.RFD()
 	if err != nil {
-		c.LogWarnf("BPF offload: RFD: %v", err)
+		slog.Warn("BPF offload: RFD error", "error", err, "ConnLink", c)
 		return false
 	}
 	lcookie, err := c.LSOCookie()
 	if err != nil {
-		c.LogWarnf("BPF offload: LSOCookie: %v", err)
+		slog.Warn("BPF offload: LSOCookie error", "error", err, "ConnLink", c)
 		return false
 	}
 	rcookie, err := c.RSOCookie()
 	if err != nil {
-		c.LogWarnf("BPF offload: RSOCookie: %v", err)
+		slog.Warn("BPF offload: RSOCookie error", "error", err, "ConnLink", c)
 		return false
 	}
 
 	if err := b.Sockmap.Add(lfd, rfd, lcookie, rcookie); err != nil {
-		c.LogWarnf("BPF offload: b.Sockmap.Add: %v", err)
+		slog.Warn("BPF offload: b.Sockmap.Add error", "error", err, "ConnLink", c)
 		return false
 	}
 
 	c.Offloaded = true
-	c.LogInfo("BPF sockmap offload activated")
+	slog.Info("BPF sockmap offload activated", "ConnLink", c)
 	return true
 }
 
@@ -116,12 +117,12 @@ func (b *BPF) DeleteOffload(c *common.ConnLink) {
 	}
 	lcookie, err := c.LSOCookie()
 	if err != nil {
-		c.LogWarnf("BPF delete offload: LSOCookie: %v", err)
+		slog.Warn("BPF delete offload: LSOCookie error", "error", err, "ConnLink", c)
 		return
 	}
 	rcookie, err := c.RSOCookie()
 	if err != nil {
-		c.LogWarnf("BPF delete offload: RSOCookie: %v", err)
+		slog.Warn("BPF delete offload: RSOCookie error", "error", err, "ConnLink", c)
 		return
 	}
 	b.Sockmap.Delete(lcookie, rcookie)
